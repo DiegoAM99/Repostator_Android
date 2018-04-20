2174075c8660ccbb2174ab7f1cf566404b096cac
xp <alvarezmd@hotmail.es>
2018-04-20 12:12:44 +0200

    APP molona

---
+++ app/src/main/java/com/example/xp/repostator_android/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.xp.repostator_android;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Intent ventanaCaptura = new Intent(this, Entrada_Datos.class);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(ventanaCaptura);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ArrayList<String> listadoRepostajes = new ArrayList<String>();
+        ListView listaVista;
+        SharedPreferences sp = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        int size = sp.getInt("Listado_size", 0);
+
+        String precio, kilometros, litros, dia,mes,anyo;
+        double costeTotal;
+        for (int i=1; i<=size; i++){
+            precio = sp.getString("precio" +i,"0");
+            kilometros = sp.getString("kilometros" +i,"0");
+            litros = sp.getString("litros" +i,"0");
+            costeTotal = Double.valueOf(precio) * Double.valueOf(litros);
+
+            dia = String.valueOf(sp.getInt("dia_"+i,0));
+            mes = String.valueOf(sp.getInt("mes_"+i,0));
+            anyo = String.valueOf(sp.getInt("anyo_"+i,0));
+
+            listadoRepostajes.add(kilometros + "km " + litros + " L " + precio + " €/L, coste: " + String.format("%.2f", costeTotal)
+            + "fecha: " + dia+"-" + mes + "-" +anyo);
+        }
+
+        listaVista = (ListView) findViewById(R.id.marcoLista);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                listadoRepostajes
+        );
+        listaVista.setAdapter(arrayAdapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/xp/repostator_android/Repostaje.java
@@ -0,0 +1,75 @@
+package com.example.xp.repostator_android;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Repostaje extends Fragment {
+
+
+    public Repostaje() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_repostaje, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        final EditText editPrecio = (EditText) getActivity().findViewById(R.id.precio);
+        editPrecio.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                if(i == EditorInfo.IME_ACTION_DONE){
+                    escribeDatos();
+                    getActivity().finish();
+
+                }
+
+                return false;
+            }
+        });
+    }
+    private void escribeDatos(){
+        EditText editPrecio = (EditText) getActivity().findViewById(R.id.precio);
+        EditText editKilometros = (EditText) getActivity().findViewById(R.id.kilometros);
+        EditText editLitros = (EditText) getActivity().findViewById(R.id.litros);
+        DatePicker editFecha = (DatePicker) getActivity().findViewById(R.id.fecha);
+
+        SharedPreferences sp = getActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        int size = sp.getInt("Listado_size",0);
+        size++;
+
+        editor.putString("precio_" + size, editPrecio.getText().toString());
+        editor.putString("kilometros_" + size, editKilometros.getText().toString());
+        editor.putString("litros_" + size, editLitros.getText().toString());
+
+        editor.putInt("dia_"+size, editFecha.getDayOfMonth());
+        editor.putInt("mes_"+size, editFecha.getMonth());
+        editor.putInt("anyo_"+size, editFecha.getYear());
+
+        editor.putInt("Listado_size", size);
+        editor.commit();
+    }
+}
